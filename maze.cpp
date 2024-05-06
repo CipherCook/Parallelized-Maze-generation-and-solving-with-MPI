24339678b2ba128eb2badf4b00327651694de3bb
rajatgolechha3319 <rajatgolechha13@gmail.com>
2024-05-07 01:29:40 +0530

    bcast

--- maze.cpp
+++ maze.cpp
@@ -1,6 +1,4 @@
 #include "generator/mazegenerator.hpp"
-#include "generator/bfs.hpp"
-
 #define MAZE_SIZE 32
 
 //mpicxx -g -O2 -std=c++17 -o maze maze.cpp
@@ -13,9 +11,10 @@ int main(int argc, char* argv[]) {
     int my_rank, comm_sz;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-
+    vector<vector<char>> maze_global(2 * MAZE_SIZE, vector<char>(2 * MAZE_SIZE));
     // Parse command line arguments
     char *graph_algo = NULL;
+    char *maze_solve = NULL;
 
 
     // Assign algorithm options
@@ -23,18 +22,24 @@ int main(int argc, char* argv[]) {
         if (strcmp(argv[i], "-g") == 0) {
             graph_algo = argv[i+1];
         } 
+        else if (strcmp(argv[i], "-s") == 0) {
+            maze_solve = argv[i+1];
+        }
+        
     }
     
     // Maze Generation using Kruskal's Algorithm
     if (graph_algo != NULL && strcmp(graph_algo, "kruskal") == 0) {
-
+        
         if (my_rank != 0) {
             maze_gen_kruskal_slave(my_rank, comm_sz);
         } else {
             vector<vector<char>> maze;
             maze_gen_kruskal_master(my_rank, comm_sz, maze);
+            maze_global = maze;
         }
         MPI_Barrier(MPI_COMM_WORLD);
+
     }
 
     // Maze Generation using BFS Algorithm
@@ -44,19 +49,22 @@ int main(int argc, char* argv[]) {
         } else {
             vector<vector<char>> maze;
             maze_gen_bfs_master(my_rank, comm_sz, maze);
+            maze_global = maze;
         }
         // Apply MPI Barrier here to wait for all threads
         MPI_Barrier(MPI_COMM_WORLD);
+
     }
+    // Need to BroadCast the maze to all processes
+    for (int i = 0; i < 2 * MAZE_SIZE; i++) {
+        MPI_Bcast(&maze_global[i][0], 2 * MAZE_SIZE, MPI_CHAR, 0, MPI_COMM_WORLD);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+    // Solve Maze using DFS Algorithm
+
 
     MPI_Finalize();
 
     return 0;
 }
-
-// This should fix the issues and correctly gather the data from all processes onto process 0 for printing.
-
-
-
-
-
