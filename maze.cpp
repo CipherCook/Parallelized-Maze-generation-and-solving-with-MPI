b7e679688418f2fb28bfe8c6e78f09704516e301
Utkarsh Sharma <utkarsh.mask@gmail.com>
2024-05-07 04:23:58 +0530

    Update maze.cpp w Dfs at end

--- maze.cpp
+++ maze.cpp
@@ -1,13 +1,14 @@
 #include "generator/mazegenerator.hpp"
+#include "solver/dfs.hpp"
 #define MAZE_SIZE 32
 
 //mpicxx -g -O2 -std=c++17 -o maze maze.cpp
+//mpirun -np 4 ./maze.out -g bfs -s dfs
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
-
     int my_rank, comm_sz;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
@@ -15,7 +16,6 @@ int main(int argc, char* argv[]) {
     // Parse command line arguments
     char *graph_algo = NULL;
     char *maze_solve = NULL;
-
 
     // Assign algorithm options
     for (int i = 1; i < argc; i += 2) {
@@ -25,7 +25,6 @@ int main(int argc, char* argv[]) {
         else if (strcmp(argv[i], "-s") == 0) {
             maze_solve = argv[i+1];
         }
-        
     }
     
     // Maze Generation using Kruskal's Algorithm
@@ -39,7 +38,6 @@ int main(int argc, char* argv[]) {
             maze_global = maze;
         }
         MPI_Barrier(MPI_COMM_WORLD);
-
     }
 
     // Maze Generation using BFS Algorithm
@@ -57,13 +55,22 @@ int main(int argc, char* argv[]) {
     }
     // Need to BroadCast the maze to all processes
     for (int i = 0; i < 2 * MAZE_SIZE; i++) {
-        MPI_Bcast(&maze_global[i][0], 2 * MAZE_SIZE, MPI_CHAR, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&maze_global[i][0], 2 * MAZE_SIZE, MPI_CHAR, 0, MPI_COMM_WORLD); //takes Only O(V) time 
     }
     MPI_Barrier(MPI_COMM_WORLD);
+    vector<vector<char>> local_solved_maze = dfs(maze_global, my_rank, comm_sz);
+    // if(my_rank == 3){
+    //     for(int i=0; i<MAZE_SIZE/2; i++){
+    //         cout<<".";
+    //         for(int j=0; j<2*MAZE_SIZE; j++){
+    //             cout<<local_solved_maze[i][j]<<'.';
+    //         }
+    //         cout<<'\n';
+    //     }
+    // }
+    //combine all local_solved_maze to maze_global
     
     // Solve Maze using DFS Algorithm
-
-
     MPI_Finalize();
 
     return 0;
